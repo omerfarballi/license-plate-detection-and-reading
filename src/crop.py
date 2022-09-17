d4e5a33a32443979dcb7069d238d80aa26443aa0
Ömer Faruk Ballı <71135790+omerfarballi@users.noreply.github.com>
2022-09-17 13:17:59 +0300

    Update crop.py

--- src/crop.py
+++ src/crop.py
@@ -6,7 +6,7 @@ import cv2
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import pandas as pd
 import numpy as np
-# eğer foto siyah gelirse ona çözüm bul
+
 def crop_plate(mask_path,original_path):
     if mask_path==None:
         cropped_image_path=None
@@ -36,15 +36,13 @@ def crop_plate(mask_path,original_path):
 
         x,y,w,h = cv2.boundingRect(contors)
         print(x,y,w,h)
-        #x+200 değeri değişkendir ortalama bir değer test sonrası değiştirelecektr.
-        # cropped_image = original_img[y-400:y+h+400, x-400:x+w+400]# original_img[y-33:y+h+33, x-33:x+w+33] 1270,690   1430,740
+        
         cropped_image = original_img[y-50:y+h+150, x-750:x+w+750]
         print(cropped_image.shape)
         from PIL import Image
         im = Image.fromarray(cropped_image)
-        im.save("C:\\Users\\oozer\\Desktop\\plate\\Kamre_Foto\\Crop\\cropped_image_plate_.jpeg")
-        cropped_image_path=f'C:\\Users\\oozer\\Desktop\\plate\\Kamre_Foto\\Crop\\cropped_image_plate_{timee}.png'
+        im.save("C:\\Users\\omer\\Desktop\\plate\\Kamre_Foto\\Crop\\cropped_image_plate_.jpeg")
+        cropped_image_path=f'C:\\Users\\omer\\Desktop\\plate\\Kamre_Foto\\Crop\\cropped_image_plate_{timee}.png'
         cv2.imwrite(cropped_image_path,cropped_image)
         cv2.waitKey(10)
         return cropped_image_path
-# crop_plate('C:\\Users\\eargestajyer\\Desktop\\interface_plate\\Kamre_Foto\\Mask\\outpt_17 32 33.png','C:\\Users\\eargestajyer\\Desktop\\interface_plate\\Kamre_Foto\\Orginal\\image_6+9+2022  17+32+33.png')
