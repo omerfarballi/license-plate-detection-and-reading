f2f66ae0234be94418e3a4c80a49d9d5ed06d363
Ömer Faruk Ballı <71135790+omerfarballi@users.noreply.github.com>
2022-09-17 13:19:39 +0300

    Update png_to_text.py

--- src/png_to_text.py
+++ src/png_to_text.py
@@ -5,7 +5,7 @@ def png_to_text(crop_path):
         return result
     else:
         ocr = MMOCR(det='PS_CTW', recog='SAR')
-        results = ocr.readtext(crop_path, output='Kamre_Foto\Text\cropped_image_plate_75_text.jpg', export='C:\\Users\\oozer\\Desktop\\Kamre_Foto',print_result=True)
+        results = ocr.readtext(crop_path, output='Kamre_Foto\Text\cropped_image_plate_75_text.jpg', export='C:\\Users\\omer\\Desktop\\Kamre_Foto',print_result=True)
         result=results[0]['text']#[0]
         if len(result)==0:
             result='  '
